b36f8a1c4ca9d98b80a6677524a40adbd27fb66b
Florian <florianlg040594@outlook.fr>
2024-02-19 02:27:25 +0100

    création feedback

--- Router/allroutes.js
+++ Router/allroutes.js
@@ -2,7 +2,10 @@ import Route from "./route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),];
+    new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/Avis", "Avis", "/pages/feedback.html"),
+];
+    
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Garage V Parrot";
